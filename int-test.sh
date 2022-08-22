9fafae7677f2a73d81b48ca144115d45538d0b35
Marko Vlahovic <vlahovic@google.com>
2022-08-22 09:44:48 -0700

    Improving int-test.sh

--- int-test.sh
+++ int-test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Copyright 2021 Google LLC
 #
@@ -14,32 +14,74 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #set -u
-set -e
 
 setup_trap_handler() {
   # shellcheck disable=SC2124
-  trap "finish $1" ERR
+  trap "finish $1 $2" EXIT
 }
 
 finish() {
   # This function executes on exit and destroys the working project
-  echo "Error deploying test-setup"
-  project_id=$(terraform -chdir="$1" output -raw project_id)
-  gcloud projects delete "$project_id" --quiet
-}
+  echo "rc=$rc"
+  echo $1 $2
+  echo SAP_STATE=absent ansible-playbook -v "$1" -e @"$2"
+  SAP_STATE=absent ansible-playbook -v "$1" -e @"$2"
+  echo ansible-playbook -vv cloud_build_deployment/setup-playbooks/destroy-ssh-bastion.yaml -e @"$2"
+  ansible-playbook -vv cloud_build_deployment/setup-playbooks/destroy-ssh-bastion.yaml -e @"$2"
 
-stack_dir="stacks/$1"
-setup_directory="$stack_dir/test-setup"
-cloudbuild_config="$stack_dir/cloudbuild.yaml"
-playbook="$stack_dir/playbook.yml"
+  mkdir -p /workspace/.results
+  # In case of the passing test we just log the results
+  if [ $rc = "0" ]; then
+    echo $rc > /workspace/.results/$SAP_TEST_ID
+    ls -l /workspace/.results/
+  # In case of the failing test we keep waiting until all the tests are executed
+  else
+    echo $rc > /workspace/.results/$SAP_TEST_ID
+    ls -l /workspace/.results/
+    total_tests_completed=$(ls /workspace/results/|wc -l)
+    while [ $total_tests_completed -lt $TOTAL_TESTS ]
+    do
+      sleep 10
+      total_tests_completed=$(ls /workspace/results/|wc -l)
+    done
+  fi
+
+}
 # shellcheck disable=SC2068
-setup_trap_handler $setup_directory
+setup_trap_handler $1 $2
 
 #mkdir -p $TF_PLUGIN_CACHE_DIR
 unset TF_PLUGIN_CACHE_DIR
-terraform -chdir="${setup_directory}" init
-terraform -chdir="${setup_directory}" apply -auto-approve
 
-playbook_vars="${setup_directory}/vars.yml"
-cat $playbook_vars
-#gcloud builds submit --config ${cloudbuild_config} --substitutions _ANSIBLE_PLAYBOOK=$playbook,_ANSIBLE_VARIABLES=$playbook_vars .
+rc=0 # return code for ansible playbook
+
+echo ansible-playbook -vv cloud_build_deployment/setup-playbooks/setup-container.yaml -e @"$2"
+ansible-playbook -vv cloud_build_deployment/setup-playbooks/setup-container.yaml -e @"$2"
+pwd
+env
+rc=$?
+if [ "$rc" != "0" ]; then
+  exit $rc
+fi
+
+if [ -z "$3" ]; then
+  retry=1;
+else
+  retry=$3
+fi
+# retry playbook in case of a failure
+for i in $(seq 1 $retry); do
+  echo ansible-playbook "$1" -e @"$2"
+  # shellcheck disable=SC2068
+  ansible-playbook -v "$1" -e @"$2"
+  rc=$?
+  echo "##################################################################################"
+  echo "####################### rc=$rc" "try=$i ##############################################"
+  echo "##################################################################################"
+  echo ""
+  if [ "$rc" = "0" ]; then
+    break
+  fi
+done
+
+exit $rc
